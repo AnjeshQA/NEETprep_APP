efe0ba423de2e66d382b4e70bfa25391d40e5128
AnjeshQA <anjesh@goodeducator.com>
2026-01-03 16:01:02 +0530

    Missing Mob Complaince Checkbox elements failure & Verify_OTP button scroll down issue fixed

--- Pages/loginScreen.py
+++ Pages/loginScreen.py
@@ -1,5 +1,4 @@
 from time import sleep
-
 from Pages.BasePage import BasePage
 from Pages.HomeScreen import HomeScreen
 
@@ -8,24 +7,32 @@ class loginScreen(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
 
-    # Added arguments to receive data from the test
     def gotoLoggedIn(self, mobileNumber, otp):
-        # 1. OPTIONAL: Handle 'Get Started' if it exists (fresh install)
-        # If your app has a 'Next' or 'Get Started' button first, add it here:
-        # if self.is_element_present("get_started_ID"):
-        #     self.click("get_started_ID")
-
-        # 2. Type Mobile Number
-        # BasePage now uses 'visibility_of_element_located', so it will wait
-        # for the splash screen to disappear automatically.
+        # 1. Type Mobile Number
         self.type("mobile_number_ID", mobileNumber)
 
-        # 3. Request OTP
+        # 2. Hide Keyboard (Ensures the checkbox and buttons are reachable)
+        if self.driver.is_keyboard_shown():
+            self.driver.hide_keyboard()
+
+        # 3. Checkbox T&C
+        self.click("term_Condition_Checkbox_ID")
+
+        # 4. Request OTP
         self.click("otpButton_ID")
 
-        # 4. Handle OTP - sometimes the field takes a moment to appear after clicking
+        # 5. Handle OTP
         self.type("enterOTP_ID", otp)
-        self.click("verifyOTP_ID")
-        sleep(10)
-        return HomeScreen(self.driver)
 
+        # Give the UI a moment to register the OTP input before swiping
+        sleep(2)
+
+        # 6. COORDINATE FIX: Swipe Up (Pixel 5: 1080x2340)
+        # Pulls the 'Verify OTP' button from the bottom edge to the center
+        self.driver.swipe(540, 1800, 540, 600, 1000)
+
+        # 7. Verify and Proceed
+        self.click("verifyOTP_ID")
+
+        # Return the next page object
+        return HomeScreen(self.driver)
